28e7a95b6ab99fdfb3a04a56459191aa947bee46
TylerJSchacht <46573177+TylerJSchacht@users.noreply.github.com>
2024-12-17 00:18:03 -0500

    Fixed Tableaus.  Able to form and display

--- Solitaire/Solitaire/Solitaire.cpp
+++ Solitaire/Solitaire/Solitaire.cpp
@@ -14,42 +14,74 @@ class Card {
 	private:
         int rank;
         char suit;
-		bool isRed;
+		bool red;
 		bool faceUp;
 		
 	public:
 		Card (int _rank = 1, char _suit = 'S') {
 			rank = _rank;
 			suit = _suit;
+
+			// For simplicity's sake, it is easier for the cards to be default face down
+			faceUp = false;
+
+			// Checks to see if the inputted character for suit
+			// is lowerCase and changes it to upperCase if needed
 			if (suit > 90)
 				suit -= 32;
-			faceUp = false;
+
+			// Checks the suit to see if the card should be red or not
+			if (suit == 'D' || suit == 'H') {
+				red = true;
+			} else {
+				red = false;
+			}
 		}
+
+		#pragma region Get & Set Commands
+		
 		int getRank() {
 			return rank;
 		}
+
 		char getSuit() {
 			return suit;
 		}
+
 		bool isFaceUp() {
 			return faceUp;
+		}
+
+		bool isRed() {
+			return red;
 		}
 		void setRank(int _rank) {
 			rank = _rank;
 		}
 		void setSuit(char _suit) {
 			suit = _suit;
+
+			// Checks to see if the inputted character for suit
+			// is lowerCase and changes it to upperCase if needed
 			if (suit > 90)
 				suit -= 32;
 		}
 		void flipFaceUp(bool _faceUp = true) {
 			faceUp = _faceUp;
 		}
+
+		#pragma endregion
+
+
+		// Inputs a Card variable that is already filled out
+		// and changes this Card's values to the input's values
 		void copyCard(Card cardToCopy) {
 			rank = cardToCopy.getRank();
 			suit = cardToCopy.getSuit();
 			faceUp = cardToCopy.isFaceUp();
+			red = cardToCopy.isRed();
 		}
+
 		void printCard() {
 			if (!faceUp) {
 				cout << "****";
@@ -75,46 +107,87 @@ class Card {
 };
 
 class Tableau {
-public:
+	public:
 	vector<Card> stack;
+	Tableau() {
+	
+	}
+	
+	void updateLastCard() {
+		stack.back().flipFaceUp(true);
+	}
+	
+	int size() {
+		return (int)stack.size();
+	}
+	
+	void PrintTableau(int row) {
+		if (stack.size() > row) {
+			stack[row].printCard();
+		}
+		else {
+			cout << "    ";
+		}
+			cout << "  ";
+	}
 };
 
 class SolitaireBoard{
-private:
-	vector<Tableau> tableaus[7];
-	vector<Card> tempStack;
+	private:
+	vector<Tableau> tableaus = {Tableau(), Tableau(), Tableau(), Tableau(), Tableau(), Tableau(), Tableau()};
+	vector<Card> inHand;
+	vector<Card> drawPile;
+	vector<Card> discardPile;
+
+
+	void AddCardToTableau(int index, vector<Card>& Deck) {
+		tableaus.at(index).stack.push_back(Card(Deck[Deck.size() -1].getRank(), Deck[Deck.size()-1].getSuit()));
+		Deck.pop_back();
+	}
+
 public:
 	SolitaireBoard(){
 	}
-	void Setup(vector<Card>& Deck) {
-		for (int tableauIndex = 0; tableauIndex > 28; tableauIndex++) {
+	void Setup(vector<Card> &Deck) {
+
+		Card tempCard;
+		for (int tableauIndex = 0; tableauIndex < 7; tableauIndex++) {
 			switch (tableauIndex) {
-			case 6: case 12: case 17: case 21: case 24: case 26: case 27: 
-				tableaus->at(6).stack.push_back(Deck.at(Deck.size() - 1));
-				break;
-			case 5: case 11: case 16: case 20: case 23: case 25: 
-				tableaus->at(5).stack.push_back(Deck.at(Deck.size() - 1));
-				break;
-			case 4: case 10: case 15: case 19: case 22: 
-				tableaus->at(4).stack.push_back(Deck.at(Deck.size() - 1));
-				break;
-			case 3: case 9: case 14: case 18: 
-				tableaus->at(3).stack.push_back(Deck.at(Deck.size() - 1));
-				break;
-			case 2: case 8: case 13: 
-				tableaus->at(2).stack.push_back(Deck.at(Deck.size() - 1));
-				break;
-			case 1: case 7: 
-				tableaus->at(1).stack.push_back(Deck.at(Deck.size() - 1));
-				break;
-			case 0: tableaus->at(0).stack.push_back(Deck.at(Deck.size() - 1));
-				break;
+			case 0: AddCardToTableau (0, Deck);
+			case 1: AddCardToTableau (1, Deck);
+			case 2: AddCardToTableau (2, Deck);
+			case 3: AddCardToTableau (3, Deck);
+			case 4: AddCardToTableau (4, Deck);
+			case 5: AddCardToTableau (5, Deck);
+			case 6: AddCardToTableau (6, Deck);
+			break;
+			default:
+				Deck[tableauIndex].printCard();
+			break;
 			}
+		}
+	}
+
+	void PrintBoard() {
+		// Need to print DrawPile, Discard Pile, and Build Piles
+		cout << "****   ";
+		int maxSize = 0;
+		for (int tableauIndex = 0; tableauIndex < 7; tableauIndex++) {
+			tableaus[tableauIndex].updateLastCard();
+			if (tableaus[tableauIndex].size() > maxSize) { maxSize = tableaus[tableauIndex].size(); }
+		}
+		for (int row = 0; row < maxSize; row++) {
+			for (int tabIndex = 0; tabIndex < 7; tabIndex++) {
+				tableaus[tabIndex].PrintTableau(row);
+			}
+			cout << endl;
 		}
 	}
 };
 
 void AddCardsAndShuffleDeck(vector<Card> &Deck);
+
+void PrintDeck(vector<Card> &Deck);
 
 void SetupSolitaire(vector<Card> &Deck);
 
@@ -126,13 +199,10 @@ int main()
 	SolitaireBoard board;
 	board.Setup(Deck);
 
+	board.PrintBoard();
 
-	// Debugging displays every card within the deck from back to front
-	for (int deckIndex = Deck.size()-1; deckIndex >= 0; deckIndex--) {
-		Deck.at(deckIndex).flipFaceUp();
-		Deck.at(deckIndex).printCard();
-		std::cout << endl;
-	}
+	//PrintDeck(Deck);
+	
 	std::cout << endl << endl;
 
 	return 0;
@@ -179,7 +249,7 @@ void AddCardsAndShuffleDeck(vector<Card> &Deck) {
 
 	// Creates a one value for the deck size so the array function .size() isn't needed
 	// to be used multiple times.
-	int deckSize = Deck.size();
+	int deckSize = (int)Deck.size();
 
 	// Going through each card and randomly choosing another card in the deck
 	// and swap places with the 2nd chosen card
@@ -191,11 +261,15 @@ void AddCardsAndShuffleDeck(vector<Card> &Deck) {
 	}
 }
 
+void PrintDeck(vector<Card>& Deck) {
+	// Debugging displays every card within the deck from back to front
+	for (int deckIndex = Deck.size() - 1; deckIndex >= 0; deckIndex--) {
+		Deck.at(deckIndex).flipFaceUp();
+		Deck.at(deckIndex).printCard();
+		std::cout << endl;
+	}
+}
+
 void SetupSolitaire(vector<Card>& Deck) {
-	vector<Card> Tableau01;
-	vector<Card> Tableau02;
-	vector<Card> Tableau03;
-	vector<Card> Tableau04;
-	vector<Card> Tableau05;
-	vector<Card> T
+	
 }
